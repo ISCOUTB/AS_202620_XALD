40d16f4f48f969c94b3ea798c092230e6a1dc359
dilanbejarano011 <dilanbejarano011@gmail.com>
2026-08-23 19:03:42 -0500

    feat: setup walking skeleton with passing unit test

---
+++ app/src/main/java/com/proyecto/xald/syncqueue/sync.kt
@@ -0,0 +1,4 @@
+package com.proyecto.xald.syncqueue
+
+class sync {
+}
---
+++ app/src/test/java/com/proyecto/xald/Entornotest.kt
@@ -0,0 +1,12 @@
+package com.proyecto.xald
+
+import org.junit.Test
+import org.junit.Assert.assertTrue
+
+class Entornotest{
+    @Test
+    fun el_entorno_base_del_monolito_modular_compila_correctamente() {
+        val sistemaConfigurado = true
+        assertTrue(sistemaConfigurado)
+    }
+}
